f97ebd13dc344c8e6ab212f0b9bcdadd287fd8d3
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-03-16 20:50:20 -0700

    added git ignore and added synchronization to handle critical sections

--- webcrawler.py
+++ webcrawler.py
@@ -1,14 +1,16 @@
 from concurrent.futures import ThreadPoolExecutor
 from bs4 import BeautifulSoup
 from collections import deque
+from threading import Lock
 import requests
 import sys
 import logging
 import time
 
 
-queue = deque()
-visited_urls = set()
+visited_url_lock = Lock()
+queue_add_lock = Lock()
+queue_poll_lock = Lock()
 error_log = "error_logs.log"
 logging.basicConfig(filename = error_log, format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
 logger = logging.getLogger()
@@ -24,63 +26,110 @@ def get_html(url):
     finally:
         return html_doc
 
-def find_duplicate_url(url):
-    if url in visited_urls:
+def add_to_visited_urls(url,visited_urls):
+    add_url = url
+    if url.startswith('https://'):
+        add_url = url[8:]
+    elif url.startswith('http://'):
+        add_url = url[7:]
+
+    if add_url.endswith('/'):
+        add_url = add_url[ :-1]
+
+    visited_url_lock.acquire()
+    visited_urls.add(add_url)
+    visited_url_lock.release()
+
+
+def find_duplicate_url(url,visited_urls):
+
+    search_string = url
+    
+    if url.startswith('https://'):
+        search_string = url[8:]
+    elif url.startswith('http://'):
+        search_string = url[7:]
+
+    if search_string.endswith('/'):
+        search_string = search_string[ :-1]
+
+    #visited_url_lock.acquire()
+    if search_string in visited_urls:
         return True
     return False
+    #visited_url_lock.release()
 
-def task(url, queue):
-    print('task getting executed')
-    links = []
-    source = get_html(url)
-    
-    if source == 'error':
-        #do not process this
+def task(url, queue, visited_urls):
+
+    visited_url_lock.acquire()
+    if find_duplicate_url(url,visited_urls):
+        visited_url_lock.release()
         pass
     else:
-        soup = BeautifulSoup(source,'lxml')
-        for link in soup.find_all('a'):
-            href_string = link.get('href')
-            if href_string is not None and (href_string.startswith('http') or href_string.startswith('https')):
-                #visited_urls.add(href_string)
-                links.append(href_string)
-                #queue.append(href_string)
-        visited_urls.add(url)
-        print(url)
-        for link in links:
-            print("\t"+link)
-            if find_duplicate_url(link):
-                pass
-            else:
-                queue.append(link)
+        visited_url_lock.release()
+        links = []
+        source = get_html(url)
+        
+        if source == 'error':
+            #do not process this
+            pass
+        else:
+            soup = BeautifulSoup(source,'lxml')
+            for link in soup.find_all('a'):
+                href_string = link.get('href')
+                if href_string is not None and (href_string.startswith('http') or href_string.startswith('https')):
+                    links.append(href_string)
+            print(url)
+            add_to_visited_urls(url,visited_urls)
+            #visited_urls.add(url)
+            for link in links:
+                print("\t"+link)
+                visited_url_lock.acquire()
+                if find_duplicate_url(link,visited_urls):
+                    #print("alread visited"+link)
+                    visited_url_lock.release()
+                    continue
+                else:
+                    visited_url_lock.release()
+                    #queue.append(link)
+                    add_to_queue(link,queue)
             
 
+def add_to_queue(url,queue):
+    queue_add_lock.acquire()
+    queue.append(url)
+    queue_add_lock.release()
 
 
 
 def find_links(queue,visited_urls):
     executor = ThreadPoolExecutor(max_workers = 20)
-    #count = 0
 
     while(True):
         while (len(queue) != 0):
+            queue_poll_lock.acquire()
             url = queue.popleft()
-            #print(++count)
-            executor.submit(task(url,queue))
+            queue_poll_lock.release()
+            executor.submit(task(url,queue, visited_urls))
         executor.shutdown(wait = True)
-        #time.sleep(10)
         if(len(queue)==0):
             break
 
 
-
 def main():
+    queue = deque()
+    visited_urls = set()
     url = sys.argv[1];
-    visited_urls.add(url)
-    queue.append(url)
+    add_to_visited_urls(url,visited_urls)
+    #visited_urls.add(url)
+    #print(visited_urls)
+    #queue.append(url)
+    add_to_queue(url,queue)
     #for the first time we have to populate links so that each thread can work on them
     links = []
+    #queue_poll_lock.acquire()
     url = queue.popleft()
+    #queue_poll_lock.release()
     source = get_html(url)
     if source == 'error':
         #do not process initial step itself
@@ -91,17 +140,21 @@ def main():
         for link in soup.find_all('a'):
             href_string = link.get('href')
             if href_string is not None and (href_string.startswith('http') or href_string.startswith('https')):
-                    #visited_urls.add(href_string)
-                    links.append(href_string)
-                    #queue.append(href_string)
+                links.append(href_string)
+                    
         print(url)
-        visited_urls.add(url)
+        #visited_urls.add(url)
+        add_to_visited_urls(url,visited_urls)
         for link in links:
             print("\t"+link)
-            if find_duplicate_url(link):
+            visited_url_lock.acquire()
+            if find_duplicate_url(link,visited_urls):
+                visited_url_lock.release()
                 continue
-            else
-                queue.append(link)
+            else:
+                #queue.append(link)
+                visited_url_lock.release()
+                add_to_queue(link,queue)
         find_links(queue,visited_urls)
 
 
